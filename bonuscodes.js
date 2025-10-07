36e960525e39f546355775102ff2995bafa8a35a
KnödelBrot <knoedelbrothost@gmail.com>
2025-10-07 16:33:58 +0200

    Add files via upload

--- bonuscodes.js
+++ bonuscodes.js
@@ -1,20 +1,18 @@
 // bonuscodes.js
-const bonusCodes = [
+export const bonusCodes = [
     { code: "test123456789", expires: "2099-10-01", typ: "ultra" },
     { code: "benni-571", expires: "2026-01-01", typ: "partner" },
-    { code: "4anos57", expires: "2025-11-01", typ: "basic"},
-
+    { code: "4anos57", expires: "2025-11-01", typ: "basic" }
 ];
 
-// Funktion: prüft, ob Code gültig ist
-function isBonusCodeValid(inputCode) {
+// Funktion: prüft, ob Code gültig ist und gibt den Typ zurück
+export function getBonusType(inputCode) {
     const today = new Date();
-    for(const b of bonusCodes){
+    for (const b of bonusCodes) {
         const exp = new Date(b.expires);
-        if(b.code === inputCode && today <= exp){
-            return true;
+        if (b.code === inputCode && today <= exp) {
+            return b.typ;
         }
     }
-    return false;
+    return "none"; // kein gültiger Code
 }
-
